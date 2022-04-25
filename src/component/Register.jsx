a8320d9b322f52f8651e9dfc131261b1008fd764
hieu tran <chanerdame@gmail.com>
2022-04-25 12:10:09 +0700

    update login

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -4,9 +4,7 @@ const Register = () => {
             <div className="container py-5 h-100">
                 <div className="row d-flex align-items-center justify-content-center h-100">
                     <div className="col-md-8 col-lg-7 col-xl-6">
-                        <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                            className="img-fluid" alt="Phone image" />
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="img-fluid" alt="Login"/>
                     </div>
                     <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
                         <form>
